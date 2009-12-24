ed2b0cb440f16e89b631f2fa69740b8ba4e3447c
DmitryZamkov <dmitry94@gmail.com>
2009-12-24 18:54:20 -0500

    Finally decided on a way to store data in Reality

--- ChronoTank/src/nullity/engine/enginereality.cpp
+++ ChronoTank/src/nullity/engine/enginereality.cpp
@@ -3,6 +3,8 @@
 /************************************************/
 #include <assert.h>
 #include <limits>
+#include <map>
+#include <set>
 #include <stack>
 
 #include "enginereality.h"
@@ -28,23 +30,26 @@ TimeStep Reality::GetStart() {
 
 //--
 TimeStep Reality::GetEnd() {
-	return this->_timeblock.End;
+	return this->_end;
 }
 
 //--
 void Reality::Init() {
 	this->_source = NULL;
-	this->_timeblock.End = -std::numeric_limits<TimeStep>::infinity();
-	this->_timeblock.Prev = NULL;
-	this->_timeblock.Child = NULL;
+	this->_end = -std::numeric_limits<TimeStep>::infinity();
 }
 
 //--
 void Reality::Destroy() {
-	for(std::vector<Reality*>::iterator it = this->_derived.begin();
+	for(std::set<Reality*>::iterator it = this->_derived.begin();
 		it != this->_derived.end(); it++)
 	{
-		(*it)->Destroy();
+		Reality* d = *it;
+		d->_source = NULL;
+		d->Destroy();
+	}
+	if(this->_source != NULL) {
+		this->_source->_derived.erase(this);
 	}
 	delete this;
 }
@@ -52,20 +57,80 @@ void Reality::Destroy() {
 //--
 Reality* Reality::Split() {
 	Reality* nr = new Reality();
-	nr->_timeblock.End = this->GetEnd();
-	nr->_timeblock.Prev = &this->_timeblock;
-	nr->_timeblock.Child = NULL;
 	nr->_source = this;
-	this->_derived.push_back(nr);
+	nr->_end = this->_end;
+	this->_derived.insert(nr);
 	return nr;
 }
 
 //--
-void Reality::RecordState(TimeStep Time, IObject* Object) {
+Reality* Reality::Cut(TimeStep Time) {
+	// Check for valid request
+	assert(Time > this->GetStart() && Time <= this->GetEnd());
 
+	// Meta-data
+	Reality* nr = new Reality();
+	nr->_source = this;
+	nr->_end = this->_end;
+	this->_derived.insert(nr);
+
+	// Entity information
+	for(_datatype::iterator it = this->_data.begin();
+		it != this->_data.end(); it++)
+	{
+		IEntity* e = (*it).first;
+		_histtype& h = (*it).second;
+		std::vector<_histtype::iterator> inforemove;
+		for(_histtype::iterator hit = h.begin();
+			hit != h.end(); hit++)
+		{
+			_info& i = *hit;
+			if(i.Time >= Time) {
+				nr->_data[e].push_back(i);
+				inforemove.push_back(hit);
+			}
+		}
+		for(std::vector<_histtype::iterator>::iterator hiit = inforemove.begin();
+			hiit != inforemove.end(); hiit++)
+		{
+			h.erase(*hiit);
+		}
+	}
+
+	return nr;
 }
 
 //--
-void Reality::RecordRemoval(TimeStep Time, IObject* Object) {
-	
+void Reality::Merge() {
+	assert(this->_derived.size() == 1);
+
+	// Meta-data
+	Reality* r = *(this->_derived.begin());
+	this->_derived.empty();
+	this->_end = r->_end;
+
+	// Entity information
+	for(_datatype::iterator it = r->_data.begin();
+		it != r->_data.end(); it++)
+	{
+		IEntity* e = (*it).first;
+		_histtype& h = (*it).second;
+		for(_histtype::iterator hit = h.begin();
+			hit != h.end(); hit++)
+		{
+			_info& i = *hit;
+			this->_data[e].push_back(i);
+		}
+	}
+
+	// Reality relation information
+	for(std::set<Reality*>::iterator dit = r->_derived.begin();
+		dit != r->_derived.end(); dit++)
+	{
+		Reality* d = *dit;
+		d->_source = this;
+		this->_derived.insert(d);
+	}
+
+	delete r;
 }
--- ChronoTank/src/nullity/engine/enginereality.h
+++ ChronoTank/src/nullity/engine/enginereality.h
@@ -2,12 +2,14 @@
 #define NULLITY_ENGINE_REALITY_H
 
 #include <vector>
+#include <set>
 #include <map>
 
 #include "../eventlog.h"
 #include "../world.h"
 
 namespace nullity {
+	class Reality;
 
 	/// An implemented reality.
 	class Reality : public IReality {
@@ -42,38 +44,21 @@ namespace nullity {
 		/// Records the state of the object at the time in this reality.
 		void		RecordState(TimeStep Time, IObject* Object);
 
-		/// Records the removal of an object.
-		void		RecordRemoval(TimeStep Time, IObject* Object);
-
 	private:
-		// Recorded entity state.
-		struct _entity_state {
-			TimeStep			Time;
-			IFrame*				Recorded;
-			IEntity*			Entity;
-			IObject*			State;	// Null if removed.
-			_entity_state*		Next;	// Next state for this entity.
+
+		// Object update information.
+		struct _info {
+			IObject*		State;
+			TimeStep		Time;
 		};
 
-		// Division of time.
-		struct _time_block {
-			std::map<IEntity*, _entity_state*>		Entities;	// Map of entities that exist throughout the entire timeblock
-																// and the state that corresponds to the the entity at the
-																// begining of the block.
-			std::map<IEntity*, _entity_state*>		Introduced; // Map of entities introduced in this time block and their
-																// first state.
-			std::map<IEntity*, _entity_state*>		Removed;	// Map of entities removed in this time block and their
-																// last state.
-			_time_block*							Prev;		// Last time block.
-			_time_block*							Child;		// Smaller time block with same ending time.
-			TimeStep								End;		// End of the time block.
-		};
+		typedef		std::vector<_info>				_histtype;
+		typedef		std::map<IEntity*, _histtype>	_datatype;
 
-		Reality*								_source;
-		std::vector<Reality*>					_derived;
-		_time_block								_timeblock;
-
-		void	_add_entity_state(_entity_state* State);
+		TimeStep				_end;
+		Reality*				_source;
+		std::set<Reality*>		_derived;
+		_datatype				_data;
 	};
 
 }
